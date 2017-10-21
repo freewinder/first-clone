bc7e98532cdf698187414bcb8da52034abd3f792
freewinder <779845815@qq.com>
2017-10-21 14:26:47 +0800

    add a new clickevent

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 window.onload=function(){
 	document.getElementsByTagName('button')[0].onclick=function(){
-		document.getElementsByTagName('div')[0].style.backgroundColor = 'blue'
+		document.getElementsByTagName('div')[0].style.backgroundColor = 'blue';
+		document.getElementsByTagName("input")[0].value='请输入内容'
 	}
+	
 }
